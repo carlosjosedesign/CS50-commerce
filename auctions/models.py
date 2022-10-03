11844e301ebbab6a3c13fc60be56ca1835ed9cc3
Charles <carlosjosedesign@gmail.com>
2022-10-03 19:23:27 -0300

    Project commit

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,41 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+  
+class Category(models.Model):
+    categoryName = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.categoryName
+
+class Bid(models.Model):
+    bid = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="userBid")
+    listing = models.ForeignKey('Listing', on_delete=models.CASCADE, blank=True, null=True, related_name="listingBid")
+
+    def __str__(self):
+        return f"{self.bid} from {self.user}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=300)
+    imageUrl = models.CharField(max_length=1000)
+    price = models.ForeignKey(Bid, on_delete=models.SET_NULL, blank=True, null=True, related_name="bidPrice")
+    bids = models.ManyToManyField(Bid, blank=True, null=True, related_name="listingBids")
+    isActive = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="listingWatchList")
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="userComment")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, null=True, related_name="listingComment")
+    message = models.CharField(max_length=500)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} comment on {self.listing} at {self.posted_at}"
+
--- auctions/views.py
+++ auctions/views.py
@@ -4,12 +4,155 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category, Listing, Bid, Comment
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    activeListings = Listing.objects.filter(isActive=True)
+    Categories = Category.objects.all()
+    return render(request, "auctions/index.html",{
+        "listings": activeListings,
+        'categories': Categories
+    })
 
+def displayCategory(request):
+    if request.method == "POST":
+        categoryFrom = request.POST["category"]
+
+        if categoryFrom == 'all':
+            return HttpResponseRedirect( reverse(index) )
+
+        category = Category.objects.get(id=categoryFrom)
+        activeListings = Listing.objects.filter(isActive=True, category=category)
+        Categories = Category.objects.all()
+        return render(request, "auctions/index.html",{
+            "listings": activeListings,
+            'categories': Categories,
+            "nowCategory": float(categoryFrom),
+        })
+
+def listing(request, id):
+    listingData = Listing.objects.get(pk=id)
+    isListingWatchList = request.user in listingData.watchlist.all()
+    allComments = Comment.objects.filter(listing=listingData)
+    isOwner = request.user.username == listingData.owner.username
+    
+    if 'message' in request.GET:
+        message = request.GET['message']
+        if 'status' in request.GET:
+            status = request.GET['status']
+    else:
+        message = False
+        status = False
+
+    return render(request, "auctions/listing.html",{
+        'listing': listingData,
+        'isListingWatchList':isListingWatchList, 
+        'allComments':allComments,
+        'isOwner':isOwner,
+        'message':message,
+        'status':status
+    })
+
+def displayWatchList(request):
+    currentUser = request.user
+    listings =currentUser.listingWatchList.all()
+    return render(request, "auctions/watchlist.html",{
+        'listings': listings,
+    })
+
+def removeWatchList(request, id):
+    listingData = Listing.objects.get(pk=id)
+    currentUser = request.user
+    listingData.watchlist.remove(currentUser)
+    return HttpResponseRedirect( reverse("listing", args=(id, ) ))
+
+def addWatchList(request, id):
+    listingData = Listing.objects.get(pk=id)
+    currentUser = request.user
+    listingData.watchlist.add(currentUser)
+    return HttpResponseRedirect( reverse("listing", args=(id, ) ))
+
+def addBid(request, id):
+    newVBid = request.POST['newBid']
+    listingData = Listing.objects.get(pk=id)
+    nowBid = 0
+    if listingData.price and listingData.price.bid:
+        nowBid = listingData.price.bid
+        
+    if int(newVBid) > nowBid:
+        updateBid = Bid(user=request.user, bid=int(newVBid), listing=listingData )
+        updateBid.save()
+        listingData.bids.add(updateBid)
+        listingData.price = updateBid
+        listingData.save()
+        return HttpResponseRedirect( reverse('listing', kwargs={'id': id})+'?message=Bid was updated successfuly&status=success' )
+    else:
+        return HttpResponseRedirect( reverse('listing', kwargs={'id': id})+'?message=Bid updated failed&status=error' )
+    
+def closeAuction(request, id):
+    listingData = Listing.objects.get(pk=id)
+    listingData.isActive = False
+    listingData.save()
+    return HttpResponseRedirect( reverse('listing', kwargs={'id': id})+'?message=Congratulations! Your auction is closed.&status=success' )
+
+def addComment(request, id):
+    currentUser = request.user
+    listingData = Listing.objects.get(pk=id)
+    message = request.POST['message']
+
+    newComment = Comment(
+        author=currentUser,
+        listing=listingData,
+        message=message
+    )
+    newComment.save()
+
+    return HttpResponseRedirect( reverse("listing", args=(id, ) ))
+
+def myAuctions(request):
+    currentUser = request.user
+    bids = currentUser.userBid.all()
+    listings = Listing.objects.filter(bids__in=bids).distinct()
+    ownAuctions = Listing.objects.filter(owner=currentUser)
+
+    return render(request, "auctions/myauctions.html",{
+        "listings": listings,
+        "ownAuctions":ownAuctions,
+    })
+
+def createListing(request):
+    if request.method == 'GET':
+        Categories = Category.objects.all();
+        return render(request, "auctions/create.html", 
+            {'categories': Categories}
+        )
+    else:
+        title = request.POST["title"]
+        description = request.POST["description"]
+        imageUrl = request.POST["imageUrl"]
+        price = request.POST["price"]
+        category = Category.objects.get(id=request.POST["category"])
+        currentUser = request.user
+
+        bid = Bid(bid=int(price), user=currentUser)
+        bid.save()
+
+        newListing = Listing(
+            title=title,
+            description=description,
+            imageUrl=imageUrl,
+            price=bid,
+            category=category,
+            owner=currentUser
+        )
+
+        newListing.save()
+
+        bid.listing = newListing
+        bid.save()
+
+        return HttpResponseRedirect( reverse(index) )
 
 def login_view(request):
     if request.method == "POST":
